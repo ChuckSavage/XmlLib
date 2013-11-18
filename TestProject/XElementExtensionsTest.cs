ecb58ae5ef69303f7fca8a90cbe621f77b78bb1d
Chuck1411 <chuck@searisen.com>
2013-11-18 15:24:38 -0700

    Added TryParse test

--- TestProject/XElementExtensionsTest.cs
+++ TestProject/XElementExtensionsTest.cs
@@ -51,5 +51,29 @@ namespace XmlLib_Test
         }
         #endregion
 
+        #region Get_FromTryParse
+
+        class Get_FromTryParse_Class
+        {
+            public string Value { get; private set; }
+            public static bool TryParse(string s, out Get_FromTryParse_Class result)
+            {
+                result = new Get_FromTryParse_Class
+                {
+                    Value = s
+                };
+                return true;
+            }
+        }
+
+        [TestMethod]
+        public void Get_FromTryParse()
+        {
+            string value = "A value to test";
+            XElement test = new XElement("Node", value);
+            Get_FromTryParse_Class result = test.Get<Get_FromTryParse_Class>("", null);
+            Assert.AreEqual(string.Compare(value, result.Value), 0);
+        }
+        #endregion
     }
 }
