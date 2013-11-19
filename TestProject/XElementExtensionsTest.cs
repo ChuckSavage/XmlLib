ee405a31804ee9928126a6fce11e0f7286e22602
Chuck1411 <chuck@searisen.com>
2013-11-19 09:03:46 -0700

    Added Get tests

--- TestProject/XElementExtensionsTest.cs
+++ TestProject/XElementExtensionsTest.cs
@@ -8,6 +8,7 @@ namespace XmlLib_Test
     [TestClass]
     public class XElementExtensionsTest
     {
+        #region Get
         #region Get_FromElement
 
         class Get_FromElement_Class
@@ -73,6 +74,61 @@ namespace XmlLib_Test
             XElement test = new XElement("Node", value);
             Get_FromTryParse_Class result = test.Get<Get_FromTryParse_Class>("", null);
             Assert.AreEqual(string.Compare(value, result.Value), 0);
+        }
+        #endregion
+
+        [TestMethod]
+        public void Get_StringValue()
+        {
+            string value = "A value to test";
+            XElement test = new XElement("Node", value);
+            string result = test.Get("", "");
+            Assert.AreEqual(string.Compare(value, result), 0);
+        }
+
+        [TestMethod]
+        public void Get_IntValue()
+        {
+            int value = 100;
+            XElement test = new XElement("Node", value);
+            int result = test.Get("", 0);
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void Get_DateTimeValue()
+        {
+            DateTime value = DateTime.Now;
+            XElement test = new XElement("Node", value);
+            DateTime result = test.Get("", DateTime.MinValue);
+            Assert.AreEqual(value.Ticks, result.Ticks);
+        }
+
+        [TestMethod]
+        public void Get_StringAttribute()
+        {
+            string value = "A value to test";
+            XElement test = new XElement("Node", new XAttribute("A", value));
+            string result = test.Get("A", "");
+            Assert.AreEqual(string.Compare(value, result), 0);
+        }
+
+        [TestMethod]
+        public void Get_IntAttribute()
+        {
+            int value = 100;
+            XElement test = new XElement("Node", new XAttribute("A", value));
+            int result = test.Get("A", 0);
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void Get_DateTimeAttribute()
+        {
+            DateTime value = DateTime.Now;
+            XElement test = new XElement("Node", new XAttribute("A", value));
+            DateTime result = test.Get("A", DateTime.MinValue);
+            Assert.AreEqual(value.Ticks, result.Ticks);
         }
         #endregion
     }
